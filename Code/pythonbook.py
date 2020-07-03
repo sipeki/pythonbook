bbb4938408040b6d054f30ebec29d7c80125e157
sipeki <sipeki@kindlen.eu>
2020-07-03 10:44:07 +0100

    Hello world

--- Code/pythonbook.py
+++ Code/pythonbook.py
@@ -1,9 +1,18 @@
-def helloworld():
+import mock
+import module
 
-    return "Hello World"
+
+#def helloworld():
+
+ #   return "Hello World"
 
 
-def helloworld2():
-    text = "Hello World"
-    return text
+#def helloworld2():
+#   text = "Hello World"
+  #  return text
 
+def parameters():
+
+    val = input("Please input something nice")
+
+    return val
--- Tests/pythonbook_test.py
+++ Tests/pythonbook_test.py
@@ -1,11 +1,22 @@
+import mock
+import module
+
 import pytest
 from Code import pythonbook
 
-def test_helloworld():
 
-    assert pythonbook.helloworld() == "Hello World"
+#def test_helloworld():
 
-def test_helloworld2():
+ #   assert pythonbook.helloworld() == "Hello World"
 
-    assert pythonbook.helloworld2() == "Hello World"
+#def test_helloworld2():
 
+ #   assert pythonbook.helloworld2() == "Hello World"
+
+def test_parameters():
+    # module.input = lambda: 'some_input'
+    # output = module.parameters()
+   # assert output == 'expected_output'
+
+    with mock.patch.object(__builtins__, 'input', lambda: 'some_input'):
+        assert pythonbook.parameters() == 'expected_output'
