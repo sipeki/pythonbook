4547c221a08345dcb65ca417a4a8643fb9bb3891
sipeki <sipeki@kindlen.eu>
2020-07-03 22:47:45 +0100

    Parameters/Operators bug fixed

--- Code/pythonbook.py
+++ Code/pythonbook.py
@@ -16,14 +16,52 @@
 
 #    return val
 
-def parametersoperators():
+# def parametersoperators():
 
-    number = input("Please input number")
-    number = input("Please input number") + number
+#    return int(input("Please input number: ")) + int(input("Please input number: "))
+
+def conditionals():
+
+    par1 = int(input("Please input a number: "))
+    par2 = int(input("Please input a number: "))
+    par3 =  input("True or False ")
+
+    if par3.lower() == "true":
+        summed = par1 + par2
+    else:
+        summed = par1 - par2
 
 
 
-    return num
+    return summed
 
-print(parametersoperators())
+
+def conditionals2():
+
+    par1 = int(input("Please input a number: "))
+    par2 = int(input("Please input a number: "))
+
+    if par1 == 0:
+        value = par2
+    elif par2 == 0:
+        value = par1
+    else:
+        value = par1 + par2
+
+    return value
+
+
+def recursion():
+
+    result = ""     # set variable value to empty
+
+    for i in range(10):     # loop ten times
+
+        if result == "":
+            result = str(conditionals2())    # append the result to variable to the next result
+        else:
+            result = result + ' ' + str(conditionals2())
+
+    return result
+
 
--- Tests/pythonbook_test.py
+++ Tests/pythonbook_test.py
@@ -12,10 +12,41 @@ from Code import pythonbook
 
 
 
-def test_parameters():
+#def test_parameters():
 
-    assert pythonbook.parameters() == "Hello World"        # Hello World
-    assert pythonbook.parameters() == ""                   # none
-    assert pythonbook.parameters() == "12334"              # 12334
-    assert pythonbook.parameters() == "$% ^ %"             # $% ^ %
-    assert pythonbook.parameters() == "  $% ^ %"           # <space><sapac>$% ^ %
+#    assert pythonbook.parameters() == "Hello World"        # Hello World
+#    assert pythonbook.parameters() == ""                   # none
+#    assert pythonbook.parameters() == "12334"              # 12334
+#    assert pythonbook.parameters() == "$% ^ %"             # $% ^ %
+#    assert pythonbook.parameters() == "  $% ^ %"           # <space><sapac>$% ^ %
+
+#def test_parametersoperators():
+
+#    assert pythonbook.parametersoperators() == 1           # 1 0
+#    assert pythonbook.parametersoperators() == 0           # 0 0
+#    assert pythonbook.parametersoperators() == 2           # 1 1
+#    assert pythonbook.parametersoperators() == -2          # -1 -1
+#    assert pythonbook.parametersoperators() == 0            # 1 -1
+#    assert pythonbook.parametersoperators() == 24           # 12 12
+
+#def test_conditionals():
+#    assert pythonbook.conditionals() == 1  # 1 0 true
+#    assert pythonbook.conditionals() == 1  # 1 0 false
+#    assert pythonbook.conditionals() == 0  # 0 0 true
+#    assert pythonbook.conditionals() == 0  # 0 0 false
+#    assert pythonbook.conditionals() == -2  # -1 -1 true
+#    assert pythonbook.conditionals() == 0  # -1 -1 false
+#    assert pythonbook.conditionals() == 14  # 12 2 true
+#    assert pythonbook.conditionals() == 10  # 12 2 false
+
+
+#def test_conditionals2():
+#    assert pythonbook.conditionals2() == 1  # 1 0
+#    assert pythonbook.conditionals2() == 1  # 0 1
+#    assert pythonbook.conditionals2() == 14  # 12 2
+
+
+def test_recursion():
+
+    assert pythonbook.recursion() == "1 1 1 1 1 1 1 1 1 1"
+    assert pythonbook.recursion() == "2 3 5 7 9 11 15 19 21 22"     # 1 1 2 1 2 3 4 3 4 5 6 5 7 8 9 10 11 10 12 10
